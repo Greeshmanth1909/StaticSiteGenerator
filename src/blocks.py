8bf2a1c0018befdc5e869e08acfbbf8ba5c3bf5d
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-04-30 11:45:30 +0530

    more block parsing

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,13 @@
+import re
 # Takes raw markdown and converts it into a list of clock strings
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ul = "unordered list"
+block_type_ol = "ordered list"
+
 def markdown_to_blocks(markdown):
     md_list = markdown.split("\n\n")
     upd_list = map(lambda x: x.strip(), md_list)
@@ -8,3 +17,33 @@ def markdown_to_blocks(markdown):
             new_list.append(item)
     return new_list
 
+def block_to_block_type(block):
+    heading_regex = r'^#{1,6} .*$'
+    code_regex = r'^```.*|\n```$'
+    quote_line_regex = r'^> .*$'
+    ul_line_regex = r'^\*|- .*$'
+    ol_line_regex = r'^\d+\. .*$'
+
+    # Match headings
+    if re.match(heading_regex, block):
+        return block_type_heading
+
+    if re.match(code_regex, block):
+        return block_type_code
+
+    # Need to split the block
+    split_block = block.split("\n")
+
+    if split_line_helper(split_block, quote_line_regex):
+        return block_type_quote
+    if split_line_helper(split_block, ul_line_regex):
+        return block_type_ul
+    if split_line_helper(split_block, ol_line_regex):
+        return block_type_ol
+    return block_type_paragraph
+
+def split_line_helper(split_line_list, regex):
+    for line in split_line_list:
+        if not re.match(regex, line):
+            return False
+    return True
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,4 +1,4 @@
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type
 import unittest
 
 class TestBlock(unittest.TestCase):
@@ -18,3 +18,19 @@ This is the same paragraph on a new line
                           "* This is a list\n* with items",
                           ]
         self.assertEqual(block_text, expected_block)
+
+    def test_block_to_block_type(self):
+        code = "``` \nprint(\"hello\")\n if something is nothing:\n    say(hi)\n```"
+        paragraph = "this is a para, laskdjlsdkjf, \n lsakdjlsdkj\n thth"
+        heading = "### hello"
+        not_heading = "####### heh"
+        quote = "> vim is great\n> line 2\n> line 3"
+        ul = "* one\n- two\n* three"
+        ol = "1. one\n2. two\n3. three"
+        self.assertEqual(block_to_block_type(code), "code")
+        self.assertEqual(block_to_block_type(paragraph), "paragraph")
+        self.assertEqual(block_to_block_type(heading), "heading")
+        self.assertEqual(block_to_block_type(not_heading), "paragraph")
+        self.assertEqual(block_to_block_type(quote), "quote")
+        self.assertEqual(block_to_block_type(ul),"unordered list")
+        self.assertEqual(block_to_block_type(ol),"ordered list")
